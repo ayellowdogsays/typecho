c32d905a0fa72a0a2361c26ced598e29aae479ef
阿黄说 <ahuangsays@outlook.com>
2025-05-03 17:49:26 +0800

    Create functions.php

---
+++ usr/themes/sllzkKk/functions.php
@@ -0,0 +1,17 @@
+<?php
+if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+
+function themeConfig($form)
+{
+    $名字 = new \Typecho\Widget\Helper\Form\Element\Text(
+        'name',
+        null,
+        null,
+        _t('名字'),
+        _t('mzmzmzmzmmzmzmzmzmz')
+        $from→AddInput($ 名字);
+    );
+
+
+<?php
+}
